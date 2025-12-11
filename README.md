6110b1927bb384ba7b2863bd3bde79fdee192f4f
zhuojade124-dot <zhuojade124@gmail.com>
2025-12-11 21:14:36 +0800

    buzhidao

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# CZ_homewrok
+运行此文件需在终端输入以下指令
+1. 进入目录：在终端中 cd 到代码所在目录（包含 Makefile 的目录）。
+2. 清理旧文件：输入指令 make clean
+3. 编译代码：输入指令 make
+    如果编译成功，终端将显示链接过程，并生成名为 matrix_test 的可执行文件。
+    ar rcs libCmatrixPro.a src/matrix_store.o src/matrix_free.o src/matrix_operation.o src/main.o
+    Linking executable matrix_test
+    gcc src/main.o libCmatrixPro.a -o matrix_test
+4. 运行程序：输入指令 ./matrix_test 程序将自动运行并在屏幕上输出矩阵运算的结果和计算时间。
--- Vector/Vector/code/src/main.c
+++ Vector/Vector/code/src/main.c
@@ -2,48 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 int main(){
-    // printf("维度为100的矩阵")；
-    // INTEGER rows_1 = 100;
-    // INTEGER cols_1 = 100;
-    // for (INTEGER i = 0;i < 10; i++){
-    //     printf("第 %d 次\n", i);
-    //     REAL alpha = i;
-    //     Matrix* A = createMatrix(rows_1, cols_1);
-    //     Matrix *B = createMatrix(rows_1, cols_1);
-    //     Matrix *add = addMatrix(A, B);
-    //     Matrix *scal = scalaire_multiplyMatrix(alpha, A);
-    //     Matrix *trans = transposeMatrix(A);
-    //     Matrix *mult = multiplyMatrix(A, B);
-        
-    //}
-    // printf("维度为1000的矩阵\n");
-    // INTEGER rows_2 = 1000;
-    // INTEGER cols_2 = 1000;
-    // for (INTEGER i = 0; i < 10; i++){
-    //     printf("第 %d 次\n", i);
-    //     REAL alpha = i;
-    //     Matrix* A = createMatrix(rows_2, cols_2);
-    //     Matrix *B = createMatrix(rows_2, cols_2);
-    //     Matrix *add = addMatrix(A, B);
-    //     Matrix *scal = scalaire_multiplyMatrix(alpha, A);
-    //     Matrix *trans = transposeMatrix(A);
-    //     Matrix *mult = multiplyMatrix(A, B);
-        
-    // }
-    printf("维度为10000的矩阵\n");
-    INTEGER rows_3 = 10000;
-    INTEGER cols_3 = 10000;
+    printf("Matrix of size 100\n");
+    INTEGER rows_1 = 100;
+    INTEGER cols_1 = 100;
+    for (INTEGER i = 0;i < 10; i++){
+        printf("第 %d 次\n", i);
+        REAL alpha = i;
+        Matrix* A = createMatrix(rows_1, cols_1);
+        Matrix *B = createMatrix(rows_1, cols_1);
+        Matrix *add = addMatrix(A, B);
+        Matrix *scal = scalaire_multiplyMatrix(alpha, A);
+        Matrix *trans = transposeMatrix(A);
+        Matrix *mult = multiplyMatrix(A, B);
+    
+    }
+    printf("维度为1000的矩阵\n");
+    INTEGER rows_2 = 1000;
+    INTEGER cols_2 = 1000;
     for (INTEGER i = 0; i < 10; i++){
         printf("第 %d 次\n", i);
         REAL alpha = i;
-        Matrix* A = createMatrix(rows_3, cols_3);
-        Matrix *B = createMatrix(rows_3, cols_3);
+        Matrix* A = createMatrix(rows_2, cols_2);
+        Matrix *B = createMatrix(rows_2, cols_2);
         Matrix *add = addMatrix(A, B);
         Matrix *scal = scalaire_multiplyMatrix(alpha, A);
         Matrix *trans = transposeMatrix(A);
         Matrix *mult = multiplyMatrix(A, B);
         
     }
+    // printf("维度为10000的矩阵\n");
+    // INTEGER rows_3 = 10000;
+    // INTEGER cols_3 = 10000;
+    // for (INTEGER i = 0; i < 10; i++){
+    //     printf("第 %d 次\n", i);
+    //     REAL alpha = i;
+    //     Matrix* A = createMatrix(rows_3, cols_3);
+    //     Matrix *B = createMatrix(rows_3, cols_3);
+    //     Matrix *add = addMatrix(A, B);
+    //     Matrix *scal = scalaire_multiplyMatrix(alpha, A);
+    //     Matrix *trans = transposeMatrix(A);
+    //     Matrix *mult = multiplyMatrix(A, B);
+        
+    // }
 
 
     return 0;
